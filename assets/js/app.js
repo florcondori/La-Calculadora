032054dd396578ad8c4d9f9100556390f816d7dd
flor condori <condorihuamanif@gmail.com>
2017-06-08 15:57:22 -0500

    calculadora operando

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,20 @@
-app
+$( _=>{
+	let screenVal = $("input:text");
+
+	$("input:button").on("click",function(){
+		var eachNumber = $(this).val();
+		var currentScreen = screenVal.val();
+
+		currentScreen += eachNumber;
+		screenVal.val(currentScreen);
+
+		if(eachNumber == "C"){
+			screenVal.val("");
+		}
+	});
+
+	$("button").click(function(e){
+		e.preventDefault();
+		screenVal.val(eval(screenVal.val()));
+	});
+});
